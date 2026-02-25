83970f558f4a5f5267541c06a5b7efeab6e70212
Patrick Bastek <patrick.bastek@stud.uni-hannover.de>
2026-02-25 03:16:49 +0100

    Added more Maps and improved Mapdistibution among the robots.

--- Source/roboExp/Private/UtilityFunctions.cpp
+++ Source/roboExp/Private/UtilityFunctions.cpp
@@ -22,15 +22,15 @@ TArray<FVector> UUtilityFunctions::genArray(FVector middle, float range, float s
 	return positions;
 }
 
-TArray<FVector> UUtilityFunctions::genGridArray(FVector middle, int32 area, int32 step)
+TArray<FVector> UUtilityFunctions::genGridArray(FVector middle, int32 maxX, int32 minX, int32 maxY, int32 minY, int32 maxZ, int32 minZ, int32 step)
 {
 	TArray<FVector> positions;
 
-	for (float i = -area; i < area; i = i + step)
-	{
-		for (float j = -area; j < area; j = j + step)
-			positions.Add(FVector(i, j, 0) + middle);
-	}
+	for (float i = -minX; i < maxX; i = i + step)
+		for (float j = -minY; j < maxY; j = j + step)
+			for(float k = -minZ ; k < maxZ; k = k + step)
+				positions.Add(FVector(i, j, k) + middle);
+	
 
 	return positions;
 }
@@ -67,8 +67,54 @@ bool UUtilityFunctions::ExportXYZ(const FString& FullPath, const TArray<FLidarPo
 		float Y = P.Location.Y;
 		float Z = P.Location.Z;
 
-		Out += FString::Printf(TEXT("%.3f %.3f %.3f\n"), X, Y, Z);
+		Out += FString::Printf(TEXT("%.3f %.3f %.3f %d %d %d\n"), X, Y, Z, P.Color.R, P.Color.G, P.Color.G);
 	}
 
 	return FFileHelper::SaveStringToFile(Out, *FullPath, FFileHelper::EEncodingOptions::ForceUTF8);
+}
+
+
+bool UUtilityFunctions::ImportXYZ(const FString& FullPath, TArray<FLidarPointCloudPoint>& Points, float UnitScale = 1.0f)
+{
+	Points.Reset();
+
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	TUniquePtr<IFileHandle> FileHandle(PlatformFile.OpenRead(*FullPath));
+	if (!FileHandle) return false;
+
+	FString FileContent;
+	if (!FFileHelper::LoadFileToString(FileContent, *FullPath))
+		return false;
+
+	TArray<FString> Lines;
+	FileContent.ParseIntoArrayLines(Lines);
+
+	for (const FString& Line : Lines)
+	{
+		TArray<FString> Tokens;
+		Line.ParseIntoArrayWS(Tokens);
+
+		if (Tokens.Num() < 3) continue;
+
+		const float X = FCString::Atof(*Tokens[0]) * UnitScale;
+		const float Y = FCString::Atof(*Tokens[1]) * UnitScale;
+		const float Z = FCString::Atof(*Tokens[2]) * UnitScale;
+
+		FLidarPointCloudPoint P(FVector3f(X, Y, Z));
+
+		if (Tokens.Num() >= 6)
+		{
+			const uint8 R = FCString::Atoi(*Tokens[3]);
+			const uint8 G = FCString::Atoi(*Tokens[4]);
+			const uint8 B = FCString::Atoi(*Tokens[5]);
+
+			P.Color = FColor(R, G, B, 255);
+			
+		}
+
+		Points.Add(P);
+
+	}
+
+	return true;
 }
--- Source/roboExp/Public/UtilityFunctions.h
+++ Source/roboExp/Public/UtilityFunctions.h
@@ -25,19 +25,22 @@ public:
 	static TArray<FVector> genArray(FVector middle, float range, float step);
 
 	UFUNCTION(BlueprintCallable, Category = "Generator")
-	static TArray<FVector> genGridArray(FVector middle, int32 area, int32 step);
+	static TArray<FVector> genGridArray(FVector middle, int32 maxX, int32 minX, int32 maxY, int32 minY, int32 maxZ, int32 minZ, int32 step);
 	
-	UFUNCTION(BlueprintCallable, Category = "Data export")
+	UFUNCTION(BlueprintCallable, Category = "Data IO")
 	static bool WriteToFile(const FString& FileName, const FString& Content);
 	
-	UFUNCTION(BlueprintCallable, Category = "Data export")
+	UFUNCTION(BlueprintCallable, Category = "Data IO")
 	static bool WriteLidarCloudToFile(
 		ULidarPointCloud* Cloud,
 		const FString& FullPath
 	);
 
-	UFUNCTION(BlueprintCallable, Category = "Data export")
+	UFUNCTION(BlueprintCallable, Category = "Data IO")
 	static bool ExportXYZ(const FString& FullPath, const TArray<FLidarPointCloudPoint>& Points, float UnitScale);
+
+	UFUNCTION(BlueprintCallable, Category = "Data IO")
+	static bool ImportXYZ(const FString& FullPath, TArray<FLidarPointCloudPoint>& Points, float UnitScale);
 
 };
 
